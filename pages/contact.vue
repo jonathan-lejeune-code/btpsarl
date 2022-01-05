7e1df43b49fe33d195a9087ddc3d0ecd15199da7
jonathan-lejeune-code <lejeune.jonathan29@outlook.fr>
2022-01-05 16:01:18 +0100

    1 commit

---
+++ pages/contact.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>je suis la</h1>
+</template>
+
+<script>
+export default {
+  name: 'ContactPage',
+}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>bievenue sur le site 2A BTP SARl</h1>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
